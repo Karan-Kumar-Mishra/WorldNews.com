dc8d58723bce1866d277eb80d743f7747b2847a5
Karan-Kumar-Mishra <emiltokaranmishra@gmail.com>
2024-09-22 20:08:30 +0530

    Update the Navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -90,16 +90,8 @@ export default function Example() {
                 transition
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
               >
-                <MenuItem>
-                  <a href="#" className="block px-4 py-2 text-sm text-black data-[focus]:bg-slate-900 data-[focus]:text-white">
-                    Your Profile
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a href="#" className="block px-4 py-2 text-sm text-black data-[focus]:bg-slate-900 data-[focus]:text-white">
-                    Settings
-                  </a>
-                </MenuItem>
+                
+               
                 <MenuItem>
                   <a onClick={logOut} className="block px-4 py-2 text-sm text-black data-[focus]:bg-slate-900 data-[focus]:text-white">
                     Sign out
